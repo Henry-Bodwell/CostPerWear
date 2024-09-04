57b00351df235a1cc5603ed4c898514647885284
Henry Bodwell <henrybodwell00@gmail.com>
2024-09-04 11:09:52 -0400

    Added clothes functions set file

--- internal/app/Clothes.go
+++ internal/app/Clothes.go
@@ -11,3 +11,21 @@ type Clothing struct {
 	costPerWear float32
 	tags        []string
 }
+// UpdateCPW calculates the value of the cost per wear variable
+// CPW = Price / Wears
+func (c *Clothing) updateCPW() {
+	c.costPerWear = c.price / float32(c.wears)
+}
+
+// incrementWears does just that
+func (c *Clothing) incrementWears() {
+	c.wears++
+}
+
+func (c *Clothing) updateImage(c.path String) {
+	c.image = path
+}
+
+func (c *Clothing) addTag(tag String) {
+	c.tag = append(c.tag, tag)
+}
